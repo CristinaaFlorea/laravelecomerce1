8047cc03cb41f84039b1757edc978b192eef86b8
CristinaaFlorea <floreacristina30@yahoo.ro>
2023-01-15 20:59:49 +0200

    commit detail

--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -6,52 +6,20 @@
     <div class="wrap-breadcrumb">
         <ul>
             <li class="item-link"><a href="#" class="link">home</a></li>
-            <li class="item-link"><span>Digital & Electronics</span></li>
+            <li class="item-link"><span>Tennis Rackets</span></li>
         </ul>
     </div>
     <div class="row">
 
         <div class="col-lg-9 col-md-8 col-sm-8 col-xs-12 main-content-area">
 
-            <div class="banner-shop">
-                <a href="#" class="banner-link">
-                    <figure><img src="{{ asset('assets/images/shop-banner.jpg')}}" alt=""></figure>
-                </a>
-            </div>
 
             <div class="wrap-shop-control">
 
-                <h1 class="shop-title">Digital & Electronics</h1>
+                <h1 class="shop-title">Tennis Rackets</h1>
 
                 <div class="wrap-right">
 
-                    <div class="sort-item orderby ">
-                        <select name="orderby" class="use-chosen" >
-                            <option value="menu_order" selected="selected">Default sorting</option>
-                            <option value="popularity">Sort by popularity</option>
-                            <option value="rating">Sort by average rating</option>
-                            <option value="date">Sort by newness</option>
-                            <option value="price">Sort by price: low to high</option>
-                            <option value="price-desc">Sort by price: high to low</option>
-                        </select>
-                    </div>
-
-                    <div class="sort-item product-per-page">
-                        <select name="post-per-page" class="use-chosen" >
-                            <option value="12" selected="selected">12 per page</option>
-                            <option value="16">16 per page</option>
-                            <option value="18">18 per page</option>
-                            <option value="21">21 per page</option>
-                            <option value="24">24 per page</option>
-                            <option value="30">30 per page</option>
-                            <option value="32">32 per page</option>
-                        </select>
-                    </div>
-
-                    <div class="change-display-mode">
-                        <a href="#" class="grid-mode display-mode active"><i class="fa fa-th"></i>Grid</a>
-                        <a href="list.html" class="list-mode display-mode"><i class="fa fa-th-list"></i>List</a>
-                    </div>
 
                 </div>
 
